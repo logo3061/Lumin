3bc748bb7c44a7c73d9a5eeac750761d05326368
logo3061 <leonzw10@gmail.com>
2026-03-20 20:25:20 +0100

    ffff

--- LuminStart.py
+++ LuminStart.py
@@ -13,7 +13,7 @@ from dotenv import load_dotenv
 # Load Token aus .env (push-sicher)
 # ----------------------------
 load_dotenv()
-TOKEN = os.getenv("DISCORD_TOKEN")  # GitHub scannt nichts
+TOKEN = "MTQ4NDYxODYxMTYwNjk1MDA1OQ.Gn1bDi.UP8e0hYxFnhg3x1Gk1jQA0mNJmjx0O-3DY7JAI"  # GitHub scannt nichts
 
 # ----------------------------
 # Konstanten
